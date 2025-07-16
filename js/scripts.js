18b0727c5d0443ebc97ec284ecca726ea014e164
VSsmbrsk <kolhoznikmarek@gmail.com>
2025-07-16 20:57:09 +0300

    add mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,10 @@ $('.owl-carousel').owlCarousel({
     }
 
 })
+
+var trigger = document.getElementById('toggle');
+var box = document.getElementById('menu');
+
+toggle.addEventListener('click', function() {
+  box.classList.toggle('active');
+});
